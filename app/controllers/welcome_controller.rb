2a7cebcbd3a5faa3b657e4713bfa170e4809defa
angkiki <gabrielangzw@gmail.com>
2018-05-04 01:23:44 +0800

    added in balance sheet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,10 @@ class WelcomeController < ApplicationController
   def home
   end
 
+  def balance_sheet
+    @asset = TotalAsset.all
+    @liability = TotalLiability.all
+    @equity = TotalEquity.all
+  end
+
 end
