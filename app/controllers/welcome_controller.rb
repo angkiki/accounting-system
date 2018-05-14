6c2c216d38c77145d681fb9e1ed18be515529a22
angkiki <gabrielangzw@gmail.com>
2018-05-14 17:59:54 +0800

    sort balance sheet and pnl accounts by name

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,16 +10,17 @@ class WelcomeController < ApplicationController
     @company = Company.includes(:accounts).find(session[:selected_company_id])
 
     @pnl = @company.profit_and_loss_figure
-    @asset = @company.total_assets
-    @liability = @company.total_liabilities
-    @equity = @company.total_equities
+
+    @asset = @company.total_assets.sort_by { |a| a.name }
+    @liability = @company.total_liabilities.sort_by { |a| a.name }
+    @equity = @company.total_equities.sort_by { |a| a.name }
   end
 
   def profit_and_loss
     @company = Company.find(session[:selected_company_id])
 
-    @revenue = @company.revenues
-    @expense = @company.expenses
+    @revenue = @company.revenues.sort_by { |a| a.name }
+    @expense = @company.expenses.sort_by { |a| a.name }
   end
 
   def general_ledger
