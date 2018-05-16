3d4f5ff46810159ad7b56ba5190e3b1ad5f7c9cf
angkiki <gabrielangzw@gmail.com>
2018-05-16 11:43:23 +0800

    added ability to edit account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,11 @@
 class AccountsController < ApplicationController
   before_action :reject_if_no_company_selected
 
+  def index
+    @company = Company.find(session[:selected_company_id])
+    @accounts = @company.accounts
+  end
+
   def new
     @account = Account.new
     @company = Company.find(session[:selected_company_id])
@@ -20,6 +25,18 @@ class AccountsController < ApplicationController
   def show
     @account = Account.find(params[:id])
     @transactions = @account.transactions.sort_by { |t| t.date }
+  end
+
+  def edit
+    @company = Company.find(session[:selected_company_id])
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @account = Account.find(params[:id])
+    @account.update(account_params)
+    flash[:success] = 'Account Updated'
+    redirect_to accounts_path
   end
 
 
